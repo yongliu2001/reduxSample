9d4b49798b1237377c10105068c97b6821fb626f
Yong Liu <yong.liu2001@gmail.com>
2016-05-04 08:47:19 +1000

    redux example

---
+++ src/app.js
@@ -0,0 +1,34 @@
+
+const store = Redux.createStore(function (state, action) {
+	switch (action.type) {
+
+		case 'ADD_CARD':
+			let newCard = Object.assign({}, action.data, {
+				score: 1,
+				id: +new Date
+			});
+			return Object.assign({}, state, {
+				cards: state.cards ? state.cards.concat([newCard]) : [newCard]
+			});
+		default: 
+			return state || {};
+	}
+});
+
+
+store.subscribe(() => {
+	console.log(store.getState());
+});
+
+store.dispatch({
+	type: 'ADD_CARD',
+	data: {
+		front: 'front',
+		back: 'back'
+	}
+});
+
+store.dispatch({
+	type: 'ADD_CARD',
+	data: {}
+});
